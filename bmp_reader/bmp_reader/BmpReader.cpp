92a739a079b798db1c149af1fbc7cfdfb173d8bd
bitLight <bitlightwork@gmail.com>
2018-01-16 22:44:54 +0700

    BMP Reader

---
+++ bmp_reader/bmp_reader/BmpReader.cpp
@@ -0,0 +1,17 @@
+﻿#include "BmpReader.h"
+
+std::ifstream operator>>(std::ifstream &fin, BmpHeader &header) {
+	fin.read(reinterpret_cast<char*>(&header.signature), 2);
+	fin.read(reinterpret_cast<char*>(&header.size), 4);
+	fin.read(reinterpret_cast<char*>(&header.reserved1), 2);
+	fin.read(reinterpret_cast<char*>(&header.reserved2), 2);
+	fin.read(reinterpret_cast<char*>(&header.offset), 4);
+}
+
+
+BmpReader::BmpReader() {
+}
+
+
+BmpReader::~BmpReader() {
+}
